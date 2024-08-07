575bba0cbbe0e44dbdf631165f92276dec34e65f
UUest <uuest@protonmail.com>
2024-08-07 15:44:30 -0700

    what a day

--- src/nodefuncs.py
+++ src/nodefuncs.py
@@ -19,17 +19,17 @@ def text_node_to_html_node(text_node: TextNode) -> LeafNode:
     
     
 def split_nodes_delimiter(old_nodes: list, delimiter: str, text_type: str) -> list:
-        def create_text_nodes(node: TextNode) -> list:
-            if node.text_type != "text":
-                return [node]
-            parts = node.text.split(delimiter)
-            if len(parts) % 2 == 0:
-                raise ValueError(f"Unmatched {delimiter} found in node text: {node.text}. Invalid Markdown syntax")
-            return [
-                TextNode(part, text_type if i % 2 else node.text_type)
-                for i, part in enumerate(parts)
+    def create_text_nodes(node: TextNode) -> list:
+        if node.text_type != "text":
+             return [node]
+        parts = node.text.split(delimiter)
+        if len(parts) % 2 == 0:
+            raise ValueError(f"Unmatched {delimiter} found in node text: {node.text}. Invalid Markdown syntax")
+        return [
+            TextNode(part, text_type if i % 2 else node.text_type)
+            for i, part in enumerate(parts)
             ]
-        return [new_node for node in old_nodes for new_node in create_text_nodes(node)]
+    return [new_node for node in old_nodes for new_node in create_text_nodes(node)]
     
     
 def extract_markdown_images(text):
@@ -37,3 +37,15 @@ def extract_markdown_images(text):
 
 def extract_markdown_links(text):
     return re.findall(r"\[(.*?)\]\((.*?)\)", text)
+
+def split_nodes_image(old_nodes: list) -> list:
+    pass
+    
+def split_nodes_link(old_nodes: list) -> list:
+    pass
+
+
+
+
+
+
