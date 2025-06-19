5a296d896cdb9a57eecfe0593aac1639ded4ebc0
UUest <uuest@protonmail.com>
2025-06-19 06:18:33 -0700

    fixed errors in blockfuncs.py and htmlnode.py, corrected issue with blockquotes markdown not being handled properly

--- src/blockfuncs.py
+++ src/blockfuncs.py
@@ -12,7 +12,7 @@ def block_to_block_type(block: str) -> str:
     quote_pattern = re.compile(r'^(> .*(\n|$))+', re.MULTILINE)
     unordered_list_pattern = re.compile(r'^(\* |- ).*(\n|$)', re.MULTILINE)
     ordered_list_pattern = re.compile(r'^\s*([0-9]+)\. .+', re.MULTILINE)
-    
+
     if heading_pattern.match(block):
         return "heading"
     elif code_pattern.match(block):
@@ -38,50 +38,65 @@ def block_to_block_type(block: str) -> str:
         return "ordered_list"
     else:
         return "paragraph"
-    
+
 text_type_text = "text"
 text_type_bold = "bold"
 text_type_italic = "italic"
 text_type_code = "code"
 text_type_link = "link"
 text_type_image = "image"
-    
+
 def markdown_to_html_node(markdown: str) -> ParentNode:
     blocks = markdown_to_blocks(markdown)
     htmlnodes = list(map(lambda node: HTMLNode(block_to_block_type(node), node), blocks))
     def text_to_children(text: str) -> list:
         return list(map(lambda textnode: text_node_to_html_node(textnode), text_to_text_nodes(text)))
     def node_to_parent_node(node: HTMLNode) -> ParentNode:
-        if node.tag  == "heading":
-            val_split = node.value.split()
-            if val_split[0] == "#":
-                new_tag = "h1"
-            elif val_split[0] == "##":
-                new_tag = "h2"
-            elif val_split[0] == "###":
-                new_tag = "h3"
-            elif val_split[0] == "####":
-                new_tag = "h4"
-            elif val_split[0] == "#####":
-                new_tag = "h5"
-            elif val_split[0] == "######":
-                new_tag = "h6"
-            children = text_to_children(node.value.strip("#").strip())
+        new_tag = "div"  # Default tag
+        children = []    # Default children
+
+        if node.tag == "heading":
+            val_split = (node.value or "").split()
+            if len(val_split) > 0:
+                if val_split[0] == "#":
+                    new_tag = "h1"
+                elif val_split[0] == "##":
+                    new_tag = "h2"
+                elif val_split[0] == "###":
+                    new_tag = "h3"
+                elif val_split[0] == "####":
+                    new_tag = "h4"
+                elif val_split[0] == "#####":
+                    new_tag = "h5"
+                elif val_split[0] == "######":
+                    new_tag = "h6"
+                else:
+                    new_tag = "h1"  # Default heading
+            children = text_to_children((node.value or "").strip("#").strip())
         elif node.tag == "quote":
             new_tag = "blockquote"
-            children = text_to_children(node.value.strip(">").strip())
+            # Process multi-line quotes by removing > from each line
+            quote_lines = (node.value or "").split('\n')
+            cleaned_lines = []
+            for line in quote_lines:
+                cleaned_line = line.lstrip('> ').rstrip()
+                if cleaned_line:  # Only add non-empty lines
+                    cleaned_lines.append(cleaned_line)
+            quote_text = ' '.join(cleaned_lines)
+            children = text_to_children(quote_text)
         elif node.tag == "code":
             new_tag = "pre"
-            children = text_to_children(node.value)
+            children = text_to_children(node.value or "")
         elif node.tag == "unordered_list":
             new_tag = "ul"
-            children = list_node_to_leaf_node(node.value)
+            children = list_node_to_leaf_node(node.value or "")
         elif node.tag == "ordered_list":
             new_tag = "ol"
-            children = list_node_to_leaf_node(node.value)
+            children = list_node_to_leaf_node(node.value or "")
         elif node.tag == "paragraph":
             new_tag = "p"
-            children = text_to_children(node.value)
+            children = text_to_children(node.value or "")
+
         return ParentNode(new_tag, children)
     new_nodes = []
     for node in htmlnodes:
--- src/htmlnode.py
+++ src/htmlnode.py
@@ -7,7 +7,7 @@ class HTMLNode:
         self.value = value
         self.children = children
         self.props = props
-        
+
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, HTMLNode):
             return False
@@ -17,45 +17,47 @@ class HTMLNode:
             self.children == other.children and
             self.props == other.props
         )
-    
+
     def to_html(self):
         raise NotImplementedError
-    
+
     def props_to_html(self) -> str:
+        if self.props is None:
+            return ""
         return "".join(map(lambda item: f' {item[0]}="{item[1]}"', self.props.items()))
-    
+
     def __repr__(self) -> str:
         return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})"
 
-   
+
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
         props = props or {}
         if (value is None or value == '') and ("src" not in props or "alt" not in props):
             raise ValueError("All leaf nodes must have a value")
         super().__init__(tag=tag, value=value, children=None, props=props)
-        
+
     def __eq__(self, other: object) -> bool:
         return super().__eq__(other)
-        
+
     def to_html(self) -> str:
         if self.tag is None or self.tag == '':
-            return self.value
+            return self.value or ""
         elif self.props is None or self.props == {}:
-            return f"<{self.tag}>{self.value}</{self.tag}>"
-        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
-    
+            return f"<{self.tag}>{self.value or ''}</{self.tag}>"
+        return f"<{self.tag}{self.props_to_html()}>{self.value or ''}</{self.tag}>"
+
     def __repr__(self) -> str:
         return f"LeafNode(tag={self.tag}, value={self.value}, props={self.props})"
 
-   
+
 class ParentNode(HTMLNode):
     def __init__(self, tag, children, props=None):
         super().__init__(tag=tag, value=None, children=children, props=props)
-        
+
     def __eq__(self, other: object) -> bool:
         return super().__eq__(other)
-    
+
     def to_html(self) -> str:
         if self.tag is None or self.tag == '':
             raise ValueError("ParentNode must have a tag")
@@ -63,9 +65,6 @@ class ParentNode(HTMLNode):
             raise ValueError("ParentNode must have children")
         children_string = "".join(list(map(lambda node: node.to_html(), self.children)))
         return f"<{self.tag}>{children_string}</{self.tag}>"
-    
+
     def __repr__(self) -> str:
         return f"ParentNode(tag={self.tag}, children={self.children}, props={self.props})"
-    
-    
-    
